e54585b15c9eddd34a8a19d7d1cd4d74f4c18909
manishaac2002 <manishaac2002@gmail.com>
2024-09-24 12:06:22 +0530

    feat : Separated the components files.

---
+++ src/components/Item.js
@@ -0,0 +1,16 @@
+export default function Item({ item, onDeleteItems, onToggleItem }) {
+
+    return (
+  
+      <li>
+        <input type="checkbox" value={item.packed}
+          onChange={() => onToggleItem(item.id)} />
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+          {item.description} {item.quantity}
+        </span>
+        <button onClick={() => onDeleteItems(item.id)}>&times;</button>
+      </li>
+  
+    )
+  
+  }
